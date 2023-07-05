b140aedadf93d9b613ca5dae018673909f2123b8
Dhairya Sarvaiya <115853289+Byte717@users.noreply.github.com>
2023-07-05 16:59:51 -0400

    Update +page.svelte
    Updates the sponsors page.

--- src/routes/sponsors/+page.svelte
+++ src/routes/sponsors/+page.svelte
@@ -3,7 +3,7 @@
 </svelte:head>
 
 <h2 class="uppercase text-center text-4xl font-medium text-green-600 mt-8">
-	How to Become a Sponsor
+	<!-- How to Become a Sponsor
 </h2>
 
 <div class="border border-zinc-300 mt-4 max-w-4xl rounded mx-auto" />
@@ -16,17 +16,17 @@
 		href="/2021-2022_sponsorship_packet.pdf">here</a
 	>
 	to view the <span class="text-red-700 italic">2021 - 2022 Team 2554 Sponsorship Packet</span>
-</p>
+</p> -->
 
-<div class="border border-zinc-300 mt-10 max-w-3xl rounded mx-auto" />
+<!-- <div class="border border-zinc-300 mt-10 max-w-3xl rounded mx-auto" /> -->
 
 <h2 class="uppercase text-center text-4xl font-medium text-green-600 mt-8">
-	Our Current Sponsors!
+	Sponsors Pending!
 </h2>
 
-<p class="text-center mt-6 text-2xl font-medium">
+<!-- <p class="text-center mt-6 text-2xl font-medium">
 	We would like to thank our current sponsors for their support!
-</p>
+</p> -->
 
 <div class="border border-zinc-300 mt-6 max-w-4xl rounded mx-auto" />
 
