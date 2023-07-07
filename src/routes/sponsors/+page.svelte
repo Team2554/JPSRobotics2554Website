bc37fc4238d6d716a6859b2783a23f6451fd25d0
builderpepc <49398322+builderpepc@users.noreply.github.com>
2023-07-07 14:59:37 -0400

    update sponsor packet (#2)

--- src/routes/sponsors/+page.svelte
+++ src/routes/sponsors/+page.svelte
@@ -3,7 +3,7 @@
 </svelte:head>
 
 <h2 class="uppercase text-center text-4xl font-medium text-green-600 mt-8">
-	<!-- How to Become a Sponsor
+How to Become a Sponsor
 </h2>
 
 <div class="border border-zinc-300 mt-4 max-w-4xl rounded mx-auto" />
@@ -13,15 +13,15 @@
 		class="italic text-red-700"
 		target="_blank"
 		rel="noopener noreferrer"
-		href="/2021-2022_sponsorship_packet.pdf">here</a
+		href="/2023-2024 Sponsor Packet.pdf">here</a
 	>
-	to view the <span class="text-red-700 italic">2021 - 2022 Team 2554 Sponsorship Packet</span>
-</p> -->
+	to view the <a href="/2023-2024 Sponsor Packet.pdf" target="_blank" class="text-red-700 italic">2023 - 2024 Team 2554 Sponsorship Packet</a>
+</p>
 
 <!-- <div class="border border-zinc-300 mt-10 max-w-3xl rounded mx-auto" /> -->
 
 <h2 class="uppercase text-center text-4xl font-medium text-green-600 mt-8">
-	Sponsors Pending!
+	2023-2024 Sponsors Pending!
 </h2>
 
 <!-- <p class="text-center mt-6 text-2xl font-medium">
